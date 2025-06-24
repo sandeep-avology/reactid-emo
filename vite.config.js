149cf15fc372af33f4be73783fabe008f1698e04
frontenddev288 <frontenddev344@gmail.com>
2025-06-24 18:31:09 +0530

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/reactid-emo/"
-  
+  base: "/reactid-emo/",  
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
