3bf76dfe4e270916d5d256d6874e452ebbc02468
frontenddev288 <frontenddev344@gmail.com>
2025-06-24 18:20:16 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/vite-deploy/"
+  base:"/reactid-emo/"
   
 })
