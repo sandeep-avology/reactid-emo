cbca2e153ea19db29ddff68741090319fc455ad0
frontenddev288 <frontenddev344@gmail.com>
2025-06-25 10:51:43 +0530

    Fix Partition import case sensitivity

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/reactid-emo/",  
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  
 })
