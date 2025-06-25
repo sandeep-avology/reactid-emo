821086f2e030d75b893055891cf72bcc586c5476
frontenddev288 <frontenddev344@gmail.com>
2025-06-25 10:30:59 +0530

    Fix Partition import case sensitivity

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import HeroBanner from "./hero-bnr/HeroBanner";
-import Partition from "./partition/Partition.JSX";
 
 
 const Home = () => {
     return (
         <div className="main ">
             <HeroBanner />
-            <Partition />
         </div>
     );
 };
